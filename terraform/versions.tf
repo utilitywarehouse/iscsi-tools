cb2610b5b97f01efa854c2cda46148e0a041990a
Rob Best <robertbest89@gmail.com>
2021-03-30 11:09:24 +0100

    Use community-terraform-providers/ignition (#3)

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     ignition = {
-      source = "terraform-providers/ignition"
+      source  = "community-terraform-providers/ignition"
+      version = "< 2.0.0"
     }
   }
   required_version = ">= 0.14"
